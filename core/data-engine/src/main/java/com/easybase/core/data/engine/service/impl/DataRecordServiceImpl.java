a2c32f8906c4bb483329af02c541e7c139bd3857
Akhash R <akhash.ramprakash@liferay.com>
2025-10-29 16:28:15 +0530

    Create and use individual ResourcePermission for collections

--- core/data-engine/src/main/java/com/easybase/core/data/engine/service/impl/DataRecordServiceImpl.java
+++ core/data-engine/src/main/java/com/easybase/core/data/engine/service/impl/DataRecordServiceImpl.java
@@ -6,7 +6,6 @@
 package com.easybase.core.data.engine.service.impl;
 
 import com.easybase.context.api.util.PermissionChecker;
-import com.easybase.core.data.engine.action.CollectionActions;
 import com.easybase.core.data.engine.entity.DataRecord;
 import com.easybase.core.data.engine.service.DataRecordLocalService;
 import com.easybase.core.data.engine.service.DataRecordService;
@@ -36,28 +35,28 @@ public class DataRecordServiceImpl implements DataRecordService {
 	public DataRecord createRecord(
 		UUID tenantId, String table, Map<String, Object> data) {
 
-		_permissionChecker.check(CollectionActions.RECORD_CREATE);
+		_permissionChecker.check(table.toUpperCase() + ":CREATE");
 
 		return _dataRecordLocalService.createRecord(tenantId, table, data);
 	}
 
 	@Override
 	public void deleteRecord(UUID tenantId, String table, UUID id) {
-		_permissionChecker.check(CollectionActions.RECORD_DELETE);
+		_permissionChecker.check(table.toUpperCase() + ":DELETE");
 
 		_dataRecordLocalService.deleteRecord(tenantId, table, id);
 	}
 
 	@Override
 	public DataRecord getRecord(UUID tenantId, String table, UUID id) {
-		_permissionChecker.check(CollectionActions.RECORD_VIEW);
+		_permissionChecker.check(table.toUpperCase() + ":READ");
 
 		return _dataRecordLocalService.getRecord(tenantId, table, id);
 	}
 
 	@Override
 	public List<DataRecord> getRecords(UUID tenantId, String table) {
-		_permissionChecker.check(CollectionActions.RECORD_LIST);
+		_permissionChecker.check(table.toUpperCase() + ":READ");
 
 		return _dataRecordLocalService.getRecords(tenantId, table);
 	}
@@ -66,7 +65,7 @@ public class DataRecordServiceImpl implements DataRecordService {
 	public DataRecord updateRecord(
 		UUID tenantId, String table, UUID id, Map<String, Object> data) {
 
-		_permissionChecker.check(CollectionActions.RECORD_UPDATE);
+		_permissionChecker.check(table.toUpperCase() + ":UPDATE");
 
 		return _dataRecordLocalService.updateRecord(tenantId, table, id, data);
 	}
