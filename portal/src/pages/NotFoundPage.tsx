74e1a4a89701a3d99faee4d845341f48fd2e523f
Akhash R <akhash.ramprakash@liferay.com>
2025-11-02 23:23:28 +0530

    Add not found page

--- portal/src/pages/NotFoundPage.tsx
+++ portal/src/pages/NotFoundPage.tsx
@@ -15,9 +15,8 @@ export const NotFoundPage = () => {
         </p>
         <div className="mt-6">
           <Link
-            href="#"
+            to = "/"
             className="inline-flex items-center rounded-md bg-primary px-4 py-2 text-sm font-medium text-primary-foreground shadow-sm transition-colors hover:bg-primary/90 focus:outline-none focus:ring-2 focus:ring-primary focus:ring-offset-2"
-            prefetch={false}
           >
             Go to Homepage
           </Link>
